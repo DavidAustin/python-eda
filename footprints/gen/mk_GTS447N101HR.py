f9cdc213af87badcde0e58eba2935a2ee325eaee
Luke Cole <github@coletek.org>
2019-08-25 10:39:24 +1000

    updated GTS447N101HR fp

--- footprints/gen/mk_GTS447N101HR.py
+++ footprints/gen/mk_GTS447N101HR.py
@@ -22,21 +22,32 @@ g = FootprintGen('GTS447N101HR')
 part_w = 16
 part_h = 35.1
 
-w = 8
-h = 3
+pin_w = 6.35 # hole should perhaps be 7.35 or 7.4
+pin_h = 0.8 # hole should perhaps be 1.8
+pin_tol = 0.5 # so hole is 7.35x1.8
 
-x = 0
-y = 0
+p = 15.4
 
-g.rect_padat(x, y, w, h, '1')
+pad_offset = pin_h + pin_tol * 2
+g.rect_padat(0, pad_offset, pin_w + pin_tol * 2, pin_h + pin_tol * 2, '1', {'onsolder' : 2})
+g.rect_padat(0, pad_offset + p, pin_w + pin_tol * 2, pin_h + pin_tol * 2, '2', {'onsolder' : 2})
 
-g.rect_padat(x, 15.4, w, h, '2')
+ox1 = -pin_w / 2 - pin_tol
+oy1 = -pin_h / 2 - pin_tol
+ox2 = ox1 + pin_w + pin_tol * 2
+oy2 = oy1 + pin_h + pin_tol * 2
+g.outlinerect(ox1, oy1, ox2, oy2)
+
+ox1 = -pin_w / 2 - pin_tol
+oy1 = -pin_h / 2 + p - pin_tol
+ox2 = ox1 + pin_w + pin_tol * 2
+oy2 = oy1 + pin_h + pin_tol * 2
+g.outlinerect(ox1, oy1, ox2, oy2)
 
 ox1 = -part_w / 2.0
 oy1 = -13.1
 ox2 = ox1 + part_w
 oy2 = oy1 + part_h
-
 g.outlinerect(ox1, oy1, ox2, oy2)
 
 g.write()
