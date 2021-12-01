a68659386476997f763f7ec74624bebbe364233d
Luke Cole <github@coletek.org>
2021-12-01 13:12:11 +1000

    readded mk_peSOT23_GSD.py

--- footprints/gen/mk_peSOT23_GSD.py
+++ footprints/gen/mk_peSOT23_GSD.py
@@ -1,5 +1,5 @@
 # Python-EDA
-# Copyright (C) 2018 Luke Cole (ported), David Austin (original author)
+# Copyright (C) 2021 Luke Cole
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
