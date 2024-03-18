d07b4bca1d8a6b45389cb8fc1bc4488067f00fe1
Luke Cole <github@coletek.org>
2024-03-18 12:16:50 +1000

    corrected center for mk_SOIC-N.py and added mk_WSON-N.py tool

--- footprints/gen/mk_WSON-N.py
+++ footprints/gen/mk_WSON-N.py
@@ -1,5 +1,5 @@
 # Python-EDA
-# Copyright (C) 2018 Luke Cole
+# Copyright (C) 2024 Luke Cole
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -20,9 +20,9 @@ from footprintgen import *
 
 def make_fp(n_pins, part_w, part_h, px, py, pad_w, pad_h, thermal_pad_w = False, thermal_pad_h = False):
     if thermal_pad_w and thermal_pad_h:
-        g = FootprintGen('SOIC-%d-%.2fx%.2f-%.2fx%.2f-%.2fx%.2f-%.2fx%.2f' % (n_pins, part_w, part_h, px, py, pad_w, pad_h, thermal_pad_w, thermal_pad_h))
+        g = FootprintGen('WSON-%d-%.2fx%.2f-%.2fx%.2f-%.2fx%.2f-%.2fx%.2f' % (n_pins, part_w, part_h, px, py, pad_w, pad_h, thermal_pad_w, thermal_pad_h))
     else:
-        g = FootprintGen('SOIC-%d-%.2fx%.2f-%.2fx%.2f-%.2fx%.2f' % (n_pins, part_w, part_h, px, py, pad_w, pad_h))
+        g = FootprintGen('WSON-%d-%.2fx%.2f-%.2fx%.2f-%.2fx%.2f' % (n_pins, part_w, part_h, px, py, pad_w, pad_h))
 
     x = 0
     for i in range(1, n_pins // 2 + 1):
@@ -49,14 +49,9 @@ def make_fp(n_pins, part_w, part_h, px, py, pad_w, pad_h, thermal_pad_w = False,
 
     g.outlinerect(-ox1, -oy1, ox2, oy2)
 
-    g.outlinecirc(-ox1 + 0.3, oy1 - py - 0.3, 0.05, 0.2)
+    g.outlinecirc(-ox1 - 0.3, oy1 - py + 0.3, 0.05, 0.2)
 
     g.write()
 
-make_fp(8, 3.9, 4.9, 1.27, 6.25, 0.8, 1.5) # based on https://en.wikipedia.org/wiki/Small_Outline_Integrated_Circuit#Narrow_SOIC_(JEDEC)
-make_fp(8, 3.9, 4.9, 1.27, 5.75, 0.45, 2.15, 3.1, 2.4) # based on https://www.ti.com/lit/ds/symlink/tps54328.pdf
-make_fp(14, 3.9, 8.65, 1.27, 6.25, 0.8, 1.5) # based on https://en.wikipedia.org/wiki/Small_Outline_Integrated_Circuit#Narrow_SOIC_(JEDEC)
-make_fp(16, 3.9, 9.9, 1.27, 5.4, 0.8, 1.5) # based on https://ww1.microchip.com/downloads/en/DeviceDoc/21295d.pdf
-make_fp(16, 4.4, 10.28, 1.27, 6.25, 0.8, 1.5) # based on http://optoelectronics.liteon.com/upload/download/DS70-2009-0014/LTV-2X7%20sereis%20Mar17.PDF
-make_fp(16, 7.5, 12.8, 1.27, 8.9, 0.8, 1.5) # based on https://www.analog.com/media/en/technical-documentation/data-sheets/ADuM6020-6028.pdf
-make_fp(28, 5.3, 10.2, 0.65, 7.2, 0.45, 1.75) # based on http://ww1.microchip.com/downloads/en/DeviceDoc/20001952C.pdf
+# https://www.ti.com/lit/ds/symlink/tps62063.pdf
+make_fp(8, 2.0, 2.0, 0.5, 1.9, 0.25, 0.5, 1.6, 0.9)
