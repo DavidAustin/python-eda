e11ed9e10027c5034382c7923c71767fa73ffb63
Luke Cole <github@coletek.org>
2024-03-21 17:21:59 +1000

    updated mk_RADIAL_SMT-N.py with the first SMT version - I'm confident there is more generators in this directory that should be ported to this multi-generator

--- footprints/gen/mk_RADIAL_SMT-N.py
+++ footprints/gen/mk_RADIAL_SMT-N.py
@@ -1,5 +1,5 @@
 # Python-EDA
-# Copyright (C) 2020-2022 Luke Cole
+# Copyright (C) 2024 Luke Cole
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -22,16 +22,17 @@ class footprintradial:
     def __init__(self, name):
         self.g = FootprintGen(name)
     
-    def make_radial(self, d, p, d_hole, d_ann, origin_x = 0, origin_y = 0):
-        self.g.pinat(origin_x, origin_y, d_hole, d_ann,  1, {'square' : 1})
-        self.g.pinat(origin_x + p, origin_y, d_hole, d_ann,  2)
-    
-        self.g.outlinecirc(origin_x + p / 2.0, origin_y, d / 2.0)
+    def make_radial(self, d, p, pad_w, pad_h):
+
+        self.g.rect_padat(-p / 2.0, 0, pad_w, pad_h, 1, {'square' : 1})
+        self.g.rect_padat(p / 2.0, 0, pad_w, pad_h, 2)
+
+        self.g.outlinecirc(0, 0, d / 2.0)
 
         oo = 0.5
 
-        ox = origin_x + p / 2.0 - (d / 2.0) * math.cos(math.radians(45)) - oo
-        oy = origin_y + (d / 2.0) * math.sin(math.radians(45)) + oo
+        ox = -p / 2.0
+        oy = d / 2.0 + oo
         
         ox1 = ox - oo
         oy1 = oy
@@ -51,41 +52,11 @@ class footprintradial:
 clearance = 0.3
 
 data = {
-    '10000uF 50V UVR1H103MRD': [25, 12.5, 1.0 + clearance], # https://www.nichicon.co.jp/english/products/pdfs/e-uvr.pdf - I think this is the biggest before screw terminals are required
-    #'10000uF 6.3V UVR0J103MHD1TN': [16, 7.5, 0.8 + clearance], # https://www.nichicon.co.jp/english/products/pdfs/e-uvr.pdf
-    '2000uF 50V EKYC500ELL202ML30S' : [16, 7.5, 0.8 + clearance], # http://www.chemi-con.co.jp/cgi-bin/CAT_DB/SEARCH/cat_db_al.cgi?e=e&j=p&pdfname=kyc
-    '1000uF 50V UVR1H102MHD1TO' : [12.5, 5.0, 0.6 + clearance], # https://www.nichicon.co.jp/english/products/pdfs/e-uvr.pdf
-    '470uF 50V ESH477M050AH4AA' : [10.0, 5.0, 0.6 + clearance], # https://www.nichicon.co.jp/english/products/pdfs/e-uvr.pdf
-    '1000uF 6.3V ESK108M6R3AG3AA' : [8.0, 3.5, 0.6 + clearance], # https://api.kemet.com/component-edge/download/specsheet/ESK108M6R3AG3AA.pdf
-    '10uF 50V 860020672010' : [5.0, 2.0, 0.5 + clearance] # https://www.we-online.de/katalog/datasheet/860020672010.pdf
+    '39uF 63V A780KN396M1JLAS040' : [8.0, 3.1 + 4.2, 4.2, 2.2] # https://connect.kemet.com:7667/gateway/IntelliData-ComponentDocumentation/1.0/download/datasheet/A780KN396M1JLAS040
 }
 
 for v in data.values():
     d_ann = v[2] * 1.75
-    r = footprintradial('RADIAL-%.2f-%.2f-%.2f-%.2f' % (v[0], v[1], v[2], d_ann))
+    r = footprintradial('RADIAL_SMT-%.2f-%.2f-%.2f-%.2f' % (v[0], v[1], v[2], d_ann))
     r.make_radial(v[0], v[1], v[2], d_ann)
     r.write()
-
-# TODO - the below needs work to avoid duplicated pins - use fp/RADIAL-multi.fp for now
-'''
-r = footprintradial('RADIAL-multi')
-origin_x = 0
-origin_y = 0
-origin_dia = data['10000uF 50V UVR1H103MRD'][0]
-origin_pitch = data['10000uF 50V UVR1H103MRD'][1]
-i = 0
-
-data = sorted(data.items(), key=lambda x: x[1], reverse=True)
-
-for v in data:
-    d = v[1]
-    d_ann = d[2] * 1.75
-    if i > 0:
-        origin_x = (origin_pitch - d[1]) / 2.0
-        origin_y = (d[0] - origin_dia) / 2.0
-        r.make_radial(d[0], d[1], d[2], d_ann)
-    r.make_radial(d[0], d[1], d[2], d_ann, origin_x, origin_y)
-    r.make_radial(d[0], d[1], d[2], d_ann, origin_x, -origin_y)
-    i += 1
-r.write()
-'''
