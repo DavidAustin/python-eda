7a7c59df4ac291f881d25e2c2f2920649d5d1c43
Luke Cole <github@coletek.org>
2025-06-04 16:35:30 +1000

    added ESP32-WROOM fp and other missing parts

---
+++ footprints/gen/mk_699C124-XA6-111-tht_wrong.py
@@ -0,0 +1,261 @@
+# Python-EDA
+# Copyright (C) 2024 Luke Cole
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import math
+from footprintgen import *
+
+# based on https://www.digikey.co.uk/en/products/detail/edac-inc/699C124-2A6-111/17186511
+
+g = FootprintGen('699C124-2A6-111')
+
+h = 0.7
+w = 0.3
+
+px = 0.5
+py = -1.5/2
+
+y = py
+
+x = -px / 2.0 - px * 5
+ox1 = x - w / 2.0
+oy1 = y - h / 2.0
+ox2 = ox1 + w
+oy2 = oy1 + h
+g.outlinerect(ox1, oy1, ox2, oy2)
+g.rect_padat(x, y - h / 2, w, h * 2, 'A1')
+
+x = -px / 2.0 - px * 4
+ox1 = x - w / 2.0
+oy1 = y - h / 2.0
+ox2 = ox1 + w
+oy2 = oy1 + h
+g.outlinerect(ox1, oy1, ox2, oy2)
+g.rect_padat(x, y - h / 2, w, h * 2, 'A2')
+
+x = -px / 2.0 - px * 3
+ox1 = x - w / 2.0
+oy1 = y - h / 2.0
+ox2 = ox1 + w
+oy2 = oy1 + h
+g.outlinerect(ox1, oy1, ox2, oy2)
+g.rect_padat(x, y - h / 2, w, h * 2, 'A3')
+
+x = -px / 2.0 - px * 2
+ox1 = x - w / 2.0
+oy1 = y - h / 2.0
+ox2 = ox1 + w
+oy2 = oy1 + h
+g.outlinerect(ox1, oy1, ox2, oy2)
+g.rect_padat(x, y - h / 2, w, h * 2, 'A4')
+
+x = -px / 2.0 - px * 1
+ox1 = x - w / 2.0
+oy1 = y - h / 2.0
+ox2 = ox1 + w
+oy2 = oy1 + h
+g.outlinerect(ox1, oy1, ox2, oy2)
+g.rect_padat(x, y - h / 2, w, h * 2, 'A5')
+
+x = -px / 2.0
+ox1 = x - w / 2.0
+oy1 = y - h / 2.0
+ox2 = ox1 + w
+oy2 = oy1 + h
+g.outlinerect(ox1, oy1, ox2, oy2)
+g.rect_padat(x, y - h / 2, w, h * 2, 'A6')
+
+x = px / 2.0
+ox1 = x - w / 2.0
+oy1 = y - h / 2.0
+ox2 = ox1 + w
+oy2 = oy1 + h
+g.outlinerect(ox1, oy1, ox2, oy2)
+g.rect_padat(x, y - h / 2, w, h * 2, 'A7')
+
+x = px / 2.0 + px * 1
+ox1 = x - w / 2.0
+oy1 = y - h / 2.0
+ox2 = ox1 + w
+oy2 = oy1 + h
+g.outlinerect(ox1, oy1, ox2, oy2)
+g.rect_padat(x, y - h / 2, w, h * 2, 'A8')
+
+x = px / 2.0 + px * 2
+ox1 = x - w / 2.0
+oy1 = y - h / 2.0
+ox2 = ox1 + w
+oy2 = oy1 + h
+g.outlinerect(ox1, oy1, ox2, oy2)
+g.rect_padat(x, y - h / 2, w, h * 2, 'A9')
+
+x = px / 2.0 + px * 3
+ox1 = x - w / 2.0
+oy1 = y - h / 2.0
+ox2 = ox1 + w
+oy2 = oy1 + h
+g.outlinerect(ox1, oy1, ox2, oy2)
+g.rect_padat(x, y - h / 2, w, h * 2, 'A10')
+
+x = px / 2.0 + px * 4
+ox1 = x - w / 2.0
+oy1 = y - h / 2.0
+ox2 = ox1 + w
+oy2 = oy1 + h
+g.outlinerect(ox1, oy1, ox2, oy2)
+g.rect_padat(x, y - h / 2, w, h * 2, 'A11')
+
+x = px / 2.0 + px * 5
+ox1 = x - w / 2.0
+oy1 = y - h / 2.0
+ox2 = ox1 + w
+oy2 = oy1 + h
+g.outlinerect(ox1, oy1, ox2, oy2)
+g.rect_padat(x, y - h / 2, w, h * 2, 'A12')
+
+px = 0.5
+py = 1.5/2
+
+y = py
+
+x = -px / 2.0 - px * 5
+ox1 = x - w / 2.0
+oy1 = y - h / 2.0
+ox2 = ox1 + w
+oy2 = oy1 + h
+g.outlinerect(ox1, oy1, ox2, oy2)
+g.rect_padat(x, y + h / 2, w, h * 2, 'B12')
+
+x = -px / 2.0 - px * 4
+ox1 = x - w / 2.0
+oy1 = y - h / 2.0
+ox2 = ox1 + w
+oy2 = oy1 + h
+g.outlinerect(ox1, oy1, ox2, oy2)
+g.rect_padat(x, y + h / 2, w, h * 2, 'B11')
+
+x = -px / 2.0 - px * 3
+ox1 = x - w / 2.0
+oy1 = y - h / 2.0
+ox2 = ox1 + w
+oy2 = oy1 + h
+g.outlinerect(ox1, oy1, ox2, oy2)
+g.rect_padat(x, y + h / 2, w, h * 2, 'B10')
+
+x = -px / 2.0 - px * 2
+ox1 = x - w / 2.0
+oy1 = y - h / 2.0
+ox2 = ox1 + w
+oy2 = oy1 + h
+g.outlinerect(ox1, oy1, ox2, oy2)
+g.rect_padat(x, y + h / 2, w, h * 2, 'B9')
+
+x = -px / 2.0 - px * 1
+ox1 = x - w / 2.0
+oy1 = y - h / 2.0
+ox2 = ox1 + w
+oy2 = oy1 + h
+g.outlinerect(ox1, oy1, ox2, oy2)
+g.rect_padat(x, y + h / 2, w, h * 2, 'B8')
+
+x = -px / 2.0
+ox1 = x - w / 2.0
+oy1 = y - h / 2.0
+ox2 = ox1 + w
+oy2 = oy1 + h
+g.outlinerect(ox1, oy1, ox2, oy2)
+g.rect_padat(x, y + h / 2, w, h * 2, 'B7')
+
+x = px / 2.0
+ox1 = x - w / 2.0
+oy1 = y - h / 2.0
+ox2 = ox1 + w
+oy2 = oy1 + h
+g.outlinerect(ox1, oy1, ox2, oy2)
+g.rect_padat(x, y + h / 2, w, h * 2, 'B6')
+
+x = px / 2.0 + px * 1
+ox1 = x - w / 2.0
+oy1 = y - h / 2.0
+ox2 = ox1 + w
+oy2 = oy1 + h
+g.outlinerect(ox1, oy1, ox2, oy2)
+g.rect_padat(x, y + h / 2, w, h * 2, 'B5')
+
+x = px / 2.0 + px * 2
+ox1 = x - w / 2.0
+oy1 = y - h / 2.0
+ox2 = ox1 + w
+oy2 = oy1 + h
+g.outlinerect(ox1, oy1, ox2, oy2)
+g.rect_padat(x, y + h / 2, w, h * 2, 'B4')
+
+x = px / 2.0 + px * 3
+ox1 = x - w / 2.0
+oy1 = y - h / 2.0
+ox2 = ox1 + w
+oy2 = oy1 + h
+g.outlinerect(ox1, oy1, ox2, oy2)
+g.rect_padat(x, y + h / 2, w, h * 2, 'B3')
+
+x = px / 2.0 + px * 4
+ox1 = x - w / 2.0
+oy1 = y - h / 2.0
+ox2 = ox1 + w
+oy2 = oy1 + h
+g.outlinerect(ox1, oy1, ox2, oy2)
+g.rect_padat(x, y + h / 2, w, h * 2, 'B2')
+
+x = px / 2.0 + px * 5
+ox1 = x - w / 2.0
+oy1 = y - h / 2.0
+ox2 = ox1 + w
+oy2 = oy1 + h
+g.outlinerect(ox1, oy1, ox2, oy2)
+g.rect_padat(x, y + h / 2, w, h * 2, 'B1')
+
+# alignment holes (ground pins)
+
+p = 7.1
+d = 1.07
+#d = 0.8
+d = 0.9
+a = d * 1.2
+
+y = -1.5/2 + (1.07/2+1.15) - (1.35/2+0.7)
+g.pinat(p / 2.0, y, d, a, '0')
+g.pinat(-p / 2.0, y, d, a, '0')
+
+y = 1.5/2 - ((1.07/2+1.15) - (1.35/2+0.7))
+g.pinat(p / 2.0, y, d, a, '0')
+g.pinat(-p / 2.0, y, d, a, '0')
+
+# outline
+
+w = 8.25
+h = 3.5
+
+ox1 = -w / 2.0
+oy1 = -h/2
+ox2 = ox1 + w
+oy2 = oy1 + h
+
+g.outline(ox1, oy1, ox2, oy1)
+g.outline(ox1, oy2, ox2, oy2)
+
+g.outline(ox1, oy1, ox1, oy2)
+g.outline(ox2, oy1, ox2, oy2)
+
+g.write()
---
+++ footprints/gen/mk_W.USB3.0AM.J4-pin.py
@@ -0,0 +1,79 @@
+# Python-EDA
+# Copyright (C) 2024 Luke Cole
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import math
+from footprintgen import *
+
+# based on https://www.taobao.com/list/item/661747754877.htm
+
+g = FootprintGen('W.USB3.0AM.J4-pin')
+
+d = 1.0
+a = d * 1.2
+
+px = 2.0
+px2 = 2.5
+
+py = -1.23/2
+
+# SuperSpeed data transmission (USB3.0)
+g.pinat(-px / 2 - px2, py, d, a, '1')
+g.pinat(-px / 2, py, d, a, '2')
+g.pinat(px / 2, py, d, a, '3')
+g.pinat(px / 2 + px2, py, d, a, '4')
+
+py = 1.23/2
+
+#  USB 2.0 interface
+g.pinat(-px * 2, py, d, a, '5')
+g.pinat(-px, py, d, a, '6')
+g.pinat(0, py, d, a, '7')
+g.pinat(px, py, d, a, '8')
+g.pinat(px * 2, py, d, a, '9')
+
+# alignment holes (ground pins)
+
+p = 11.7
+
+d = 1.4
+a = d * 1.4
+y = 4.5/2 - 0.5 - 1.4/2 #+ (1.07/2+1.15) - (1.35/2+0.7)
+g.pinat(p / 2.0, y, d, a, '0')
+g.pinat(-p / 2.0, y, d, a, '0')
+
+d = 0.9
+a = d * 1.5
+y = 4.5/2 - 0.5 - 1.4 - 0.8 - 0.9/2 #+ (1.07/2+1.15) - (1.35/2+0.7)
+g.pinat(p / 2.0, y, d, a, '0')
+g.pinat(-p / 2.0, y, d, a, '0')
+
+# outline
+
+w = 12.0
+h = 4.5
+
+ox1 = -w / 2.0
+oy1 = -h/2
+ox2 = ox1 + w
+oy2 = oy1 + h
+
+g.outline(ox1, oy1, ox2, oy1)
+g.outline(ox1, oy2, ox2, oy2)
+
+g.outline(ox1, oy1, ox1, oy2)
+g.outline(ox2, oy1, ox2, oy2)
+
+g.write()
